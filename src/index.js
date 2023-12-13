b54a13f579fdc190b84f8aa1b00d7b9aa14746f7
oprecioso <diogenesprecioso@outlook.com>
2023-12-13 15:20:16 -0300

    commit do código atualizado

---
+++ src/index.js
@@ -0,0 +1,28 @@
+let nomeDoHeroi = "Super Precioso"
+let pontosDeExperiencia = 10001
+
+if(pontosDeExperiencia <= 1000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Ferro!")
+
+}else if(pontosDeExperiencia >= 1001 && pontosDeExperiencia <=2000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Bronze!")
+
+}else if(pontosDeExperiencia >= 2001 && pontosDeExperiencia <=5000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Prata!")
+
+}else if(pontosDeExperiencia >= 6001 && pontosDeExperiencia <=7000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Ouro!")
+
+}else if(pontosDeExperiencia >= 7001 && pontosDeExperiencia <=8000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Platina!")
+
+}else if(pontosDeExperiencia >= 8001 && pontosDeExperiencia <=9000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Ascendente!")
+
+}else if(pontosDeExperiencia >= 9001 && pontosDeExperiencia <=10000){
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Imortal!")
+
+}else {
+    console.log("O Herói de nome "+nomeDoHeroi,"está no Nível Radiante!")
+}
+
